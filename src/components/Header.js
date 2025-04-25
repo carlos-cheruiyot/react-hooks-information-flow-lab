67985148daf588702ae48946a001c3fa022d914b
carlos <carloskip25@gmail.com>
2025-04-25 18:18:36 +0300

    code grade done

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header({ onDarkModeClick }) {
+    return (
+      <header>
+        <h1>Shopster</h1>
+        <button onClick={onDarkModeClick}>Toggle Dark Mode</button>
+      </header>
+    );
+  }
+  
+  export default Header;
